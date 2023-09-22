c84fa3d793604658add7efc2a7e2f019c54f9836
Dmytro <moiseenkodmitriy@i.ua>
2023-09-22 23:28:31 +0300

    lesson-2

--- commander-example/app.js
+++ commander-example/app.js
@@ -1,36 +1,56 @@
-const {program} = require("commander");
+const { program } = require("commander");
 
 const books = require("./books");
 
-const invokeAction = async ({action, id, title, author}) => {
-    switch(action) {
-        case "read":
-            const allBooks = await books.getAll();
-            return console.log(allBooks);
-        case "getById":
-            const oneBook = await books.getById(id);
-            return console.log(oneBook);
-        case "add":
-            const newBook = await books.add({title, author});
-            return console.log(newBook);
-        case "updateById":
-            const updateBook = await books.updateById(id, {title, author});
-            return console.log(updateBook);
-        case "deleteById":
-            const deleteBook = await books.deleteById(id);
-            return console.log(deleteBook);
-        default: 
-            return console.log("Unknown action")
-    }
-}
+const invokeAction = async ({ action, id, title, author }) => {
+  switch (action) {
+    case "read":
+      const allBooks = await books.getAll();
+      return console.log(allBooks);
+    case "getById":
+      const oneBook = await books.getById(id);
+      return console.log(oneBook);
+    case "add":
+      const newBook = await books.add({ title, author });
+      return console.log(newBook);
+    case "updateById":
+      const updateBook = await books.updateById(id, { title, author });
+      return console.log(updateBook);
+    case "deleteById":
+      const deleteBook = await books.deleteById(id);
+      return console.log(deleteBook);
+    default:
+      return console.log("Unknown action");
+  }
+};
 
 program
-    .option("-a, --action, <type>")
-    .option("-i, --id, <type>")
-    .option("-t, --title, <type>")
-    .option("-at, --author, <type>");
+  .option("-a, --action, <type>")
+  .option("-i, --id, <type>")
+  .option("-t, --title, <type>")
+  .option("-at, --author, <type>");
 
 program.parse();
 
 const options = program.opts();
-invokeAction(options)
+
+invokeAction(options);
+
+// invokeAction({ action: "read" });
+
+// invokeAction({ action: "getById", id: "Op8ApLTY0-Vn2cR0vDIwG" });
+
+// invokeAction({
+//   action: "add",
+//   title: "You Know JS Yet",
+//   author: "John Simpson",
+// });
+
+// invokeAction({
+//   action: "updateById",
+//   id: "MowDgEQYc07JW65nS_p9b",
+//   title: "You Know JS",
+//   author: "John Simpson",
+// });
+
+// invokeAction({ action: "deleteById", id: "6gLIUh2uU6S-_yV5Lt_rX" });
